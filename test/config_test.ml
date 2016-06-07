cfeaa67c1e0e60eabb696b6ae0d33f9bd2b8be80
vagrant <vagrant@vagrant.vm>
2016-06-07 07:25:36 +0000

    Add test for function and variable

--- test/config_test.ml
+++ test/config_test.ml
@@ -31,4 +31,22 @@
      Okeyfum_config.(M.find ("a", `UP) dec) [@eq [`Id "c"]]
    end
    | None -> false [@false "Loading failure"]
+
+ let%spec "OKeyfum config should give variable definition" =
+   let conf = Okeyfum_config.load "./config.okf" in
+   match conf with
+   | Some c -> begin
+     Okeyfum_config.(Hashtbl.find (Config.variable_map c) "$var_name") [@eq [`Id "a";`Id "b";`Id "c";`Id "d"]]
+   end
+   | None -> false [@false "Loading failure"]
+
+ let%spec "OKeyfum config should be able to contain variable and function in sequence" =
+   let conf = Okeyfum_config.load "./config.okf" in
+   match conf with
+   | Some c -> begin
+     let module M = Okeyfum_config.Keydef_map in
+     let seq = Okeyfum_config.(M.find ("d", `UP) (Config.keydef_map c)) in
+     seq [@eq [`Var "$a";`Func ("fun", ["b"])]]
+   end
+   | None -> false [@false "Loading failure"]
 ]
